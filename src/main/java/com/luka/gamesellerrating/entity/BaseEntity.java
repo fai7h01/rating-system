098c49fc79c83ace8370471a11841a0d33362bb4
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-26 22:44:21 +0400

    static temp values for base entity

--- src/main/java/com/luka/gamesellerrating/entity/BaseEntity.java
+++ src/main/java/com/luka/gamesellerrating/entity/BaseEntity.java
@@ -33,4 +33,17 @@ public abstract class BaseEntity {
 
     private Boolean isDeleted = false;
 
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId = 1L;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 }
