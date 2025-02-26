85cc0855749bbe5d8c02273e86f8d6cf5f5df998
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-26 16:18:04 +0400

    mapperUtil created

--- src/main/java/com/luka/gamesellerrating/RatingSystemApplication.java
+++ src/main/java/com/luka/gamesellerrating/RatingSystemApplication.java
@@ -1,13 +1,20 @@
 package com.luka.gamesellerrating;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RatingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatingSystemApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
