5b3a0f5961eaedd32b0f724dca99115d312f9d0e
fai7h01 <l.varsimashvil10@gmail.com>
2025-03-03 19:43:27 +0400

    keycloak service added

---
+++ src/main/java/com/luka/gamesellerrating/service/KeycloakService.java
@@ -0,0 +1,11 @@
+package com.luka.gamesellerrating.service;
+
+import com.luka.gamesellerrating.dto.UserDTO;
+
+public interface KeycloakService {
+
+    void userCreate(UserDTO dto);
+    UserDTO getLoggedInUser();
+
+
+}
---
+++ src/main/java/com/luka/gamesellerrating/service/impl/KeycloakServiceImpl.java
@@ -0,0 +1,103 @@
+package com.luka.gamesellerrating.service.impl;
+
+import com.luka.gamesellerrating.config.KeycloakProperties;
+import com.luka.gamesellerrating.dto.UserDTO;
+import com.luka.gamesellerrating.service.KeycloakService;
+import com.luka.gamesellerrating.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static org.keycloak.admin.client.CreatedResponseUtil.getCreatedId;
+
+@Slf4j
+@Service
+public class KeycloakServiceImpl implements KeycloakService {
+
+    private final KeycloakProperties keycloakProperties;
+    private final UserService userService;
+
+    public KeycloakServiceImpl(KeycloakProperties keycloakProperties, UserService userService) {
+        this.keycloakProperties = keycloakProperties;
+        this.userService = userService;
+    }
+
+    @Override
+    public void userCreate(UserDTO dto) {
+
+        UserRepresentation keycloakUser = getUserRepresentation(dto);
+
+        Keycloak keycloak = getKeycloakInstance();
+
+        RealmResource realmResource = keycloak.realm(keycloakProperties.getRealm());
+        UsersResource usersResource = realmResource.users();
+
+        try {
+
+            Response result = usersResource.create(keycloakUser);
+
+            if (Objects.equals(201, result.getStatus())) {
+                String userId = getCreatedId(result);
+
+                ClientRepresentation appClient = realmResource.clients()
+                        .findByClientId(keycloakProperties.getClientId()).get(0);
+
+                RoleRepresentation userClientRole = realmResource.clients()
+                        .get(appClient.getId()).roles().get(dto.getRole().getValue()).toRepresentation();
+
+                realmResource.users().get(userId).roles().clientLevel(appClient.getId())
+                        .add(Collections.singletonList(userClientRole));
+            }
+
+        } catch (Exception e) {
+            log.error("Exception in keycloak service user create: {}", e.getMessage());
+        } finally {
+            keycloak.close();
+        }
+    }
+
+    @Override
+    public UserDTO getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SimpleKeycloakAccount userDetails = (SimpleKeycloakAccount) authentication.getDetails();
+        return userService.findByUsername(userDetails.getKeycloakSecurityContext().getToken().getPreferredUsername());
+    }
+
+    private UserRepresentation getUserRepresentation(UserDTO dto) {
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setTemporary(false);
+        credential.setValue(dto.getPassword());
+
+        UserRepresentation keycloakUser = new UserRepresentation();
+        keycloakUser.setUsername(dto.getUsername());
+        keycloakUser.setFirstName(dto.getFirstName());
+        keycloakUser.setLastName(dto.getLastName());
+        keycloakUser.setEmail(dto.getUsername());
+        keycloakUser.setCredentials(List.of(credential));
+        keycloakUser.setEmailVerified(false);
+        keycloakUser.setEnabled(true);
+        return keycloakUser;
+    }
+
+
+    private Keycloak getKeycloakInstance() {
+        return Keycloak.getInstance(keycloakProperties.getAuthServerUrl(),
+                keycloakProperties.getMasterRealm(), keycloakProperties.getMasterUser()
+                , keycloakProperties.getMasterUserPswd(), keycloakProperties.getMasterClient());
+    }
+}
