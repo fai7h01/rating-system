031adc7b0ea9805247f78593fd78b226b66d8f70
fai7h01 <l.varsimashvil10@gmail.com>
2025-03-15 04:47:35 +0400

    model mapper removed

--- src/main/java/com/luka/gamesellerrating/RatingSystemApplication.java
+++ src/main/java/com/luka/gamesellerrating/RatingSystemApplication.java
@@ -1,10 +1,8 @@
 package com.luka.gamesellerrating;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
@@ -15,10 +13,4 @@ public class RatingSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(RatingSystemApplication.class, args);
     }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
